21491d9ad49f71c91594db08fca401f88fd75c11
leandro <leandro.a.estrada@gmail.com>
2025-02-16 00:53:54 -0300

    Initial Commit

---
+++ InstaClone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  InstaClone
+//
+//  Created by leandro estrada on 16/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ InstaClone/InstaCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaCloneApp.swift
+//  InstaClone
+//
+//  Created by leandro estrada on 16/02/25.
+//
+
+import SwiftUI
+
+@main
+struct InstaCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
