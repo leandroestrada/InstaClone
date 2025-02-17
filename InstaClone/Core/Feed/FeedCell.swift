b0825c434c0c1960b645f3fd106cc1a4b251ef76
leandro <leandro.a.estrada@gmail.com>
2025-02-17 17:03:34 -0300

    Adding gitIgnore

--- InstaClone/Core/Feed/FeedCell.swift
+++ InstaClone/Core/Feed/FeedCell.swift
@@ -9,7 +9,86 @@ import SwiftUI
 
 struct FeedCell: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            // image + username
+            HStack {
+                Image("TeleProfile")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 40, height: 40)
+                    .clipShape(Circle())
+                
+                Text("Tele1")
+                    .font(.footnote)
+                    .fontWeight(.semibold)
+                
+                Spacer()
+            }
+            .padding(.leading, 8)
+            
+            
+            // post image
+            
+            Image("TeleProfile")
+                .resizable()
+                .scaledToFill()
+                .frame(height: 400)
+                .clipShape(Rectangle())
+            
+            //Action Buttons
+            HStack(spacing: 16) {
+                Button {
+                    print("Like post")
+                } label: {
+                    Image(systemName: "heart")
+                        .imageScale(.large)
+                }
+                
+                Button {
+                    print("Comment on post")
+                }  label: {
+                    Image(systemName: "bubble.right")
+                        .imageScale(.large)
+                }
+                
+                Button {
+                    print("Share post")
+                }  label: {
+                    Image(systemName: "paperplane")
+                        .imageScale(.large)
+                }
+                
+                Spacer()
+            }
+            .padding(.leading, 8)
+            .padding(.top, 4)
+            .foregroundColor(.black)
+            
+            //likes label
+            Text("23 likes")
+                .font(.footnote)
+                .fontWeight(.semibold)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.leading, 10)
+                .padding(.top, 1)
+            //caption label
+            
+            HStack {
+                Text("Tele Classic ").fontWeight(.semibold) +
+                Text("This is some test text")
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .font(.footnote)
+            .padding(.leading, 8)
+            .padding(.top, 1)
+            
+            Text("6h")
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .font(.footnote)
+                .padding(.leading, 8)
+                .foregroundColor(.gray)
+            
+        }
     }
 }
 
--- InstaClone/Core/Profile/ProfileView.swift
+++ InstaClone/Core/Profile/ProfileView.swift
@@ -8,8 +8,90 @@
 import SwiftUI
 
 struct ProfileView: View {
+    
+    private let gridItems: [GridItem] = [
+        .init(.flexible(), spacing: 1),
+        .init(.flexible(), spacing: 1),
+        .init(.flexible(), spacing: 1)
+    ]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ScrollView {
+                //Header
+                VStack(spacing: 10) {
+                    //Pic and Stats
+                    HStack {
+                        Image("TeleProfile")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 80, height: 80)
+                            .clipShape(.circle)
+                        
+                        Spacer()
+                        HStack(spacing: 8) {
+                            UserStatView(value: 3, title: "Posts")
+                            
+                            UserStatView(value: 12, title: "Followers")
+                            
+                            UserStatView(value: 24, title: "Following")
+                        }
+                    }
+                    .padding(.horizontal)
+                    
+                    
+                    //Name and Bio
+                    VStack(alignment: .leading, spacing: 4) {
+                        Text("Leandro Estrada")
+                            .font(.footnote)
+                            .fontWeight(.semibold)
+                        
+                        Text("iOS Developer")
+                            .font(.footnote)
+                        
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.horizontal)
+                    
+                    //Action Button
+                    
+                    Button {
+                        
+                    } label: {
+                        Text("Edit Profile")
+                            .font(.subheadline)
+                            .fontWeight(.semibold)
+                            .frame(width: 360, height: 32)
+                            .foregroundColor(.black)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 6).stroke(Color.gray, lineWidth: 1))
+                    }
+                    Divider()
+                }
+                
+                //MARK: Post grid view
+                
+                LazyVGrid(columns: gridItems, spacing: 1) {
+                    ForEach(0...15, id: \.self) { index in
+                        Image("TeleProfile")
+                            .resizable()
+                            .scaledToFill()
+                    }
+                }
+                
+            } .navigationTitle("Profile")
+              .navigationBarTitleDisplayMode(.inline)
+              .toolbar {
+                  ToolbarItem(placement: .navigationBarTrailing) {
+                      Button {
+                        
+                      } label: {
+                          Image(systemName: "line.3.horizontal")
+                              .foregroundColor(.black)
+                      }
+                  }
+              }
+        }
     }
 }
 
